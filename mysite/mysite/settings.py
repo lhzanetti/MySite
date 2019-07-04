20366c036546f27e2f2169bf70f2606cc1a52609
Luiz Zanetti <lhzanetti@gmail.com>
2019-07-04 14:07:07 +0000

    Primeiro Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,4 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#    '/deploy/virtualenvs/mysite/lib/python3.6/site-packages/django/contrib/admin/static',
+STATIC_ROOT = '/deploy/sites/mysite/mysite/static'
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/deploy/sites/mysite/mysite/media'
+MEDIA_URL = '/media/'
